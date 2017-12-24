c9efcb5fabfc371a02fc64049687d978e7a6d2a8
JennSung <jennifer.sung@colorado.edu>
2017-12-23 22:29:12 -0700

    Adding of update and destory actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,8 +17,24 @@ class ItemsController < ApplicationController
       image: params['image']
       )
     item.save
-
     render json: item.as_json
   end
 
+  def update
+    item = Item.find_by(id: the_id)
+    item.update(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image: params[:image]
+      )
+    render json: item.as_json
+  end
+
+  def destroy
+    item = Item.find_by(id: params[:id])
+    item.destroy
+    render json: {message: "Item successfully destroyed"}
+  end
+  
 end
--- frontend.rb
+++ frontend.rb
@@ -5,6 +5,8 @@ p "Hi. Welcome to my shop."
 p "[1] See all of my products!"
 p "[2] See a particular product."
 p "[3] Create a new product!"
+p "[4] Update a product!"
+p "[5] Delete a product"
 
 user_input = gets.chomp
 
@@ -30,4 +32,24 @@ elsif user_input == '3'
   response = Unirest.post("localhost:3000/items", parameters: the_params)
   pp response.body
 elsif user_input == '4'
+  p "Enter the id of the product you would like to update."
+  item_id = gets.chomp
+  response = Unirest.get("localhost:3000/items/#{item_id}")
+  item = response.body
+  the_params = {}
+  p "Enter the item name #{item['name']}"
+  the_params[:name] = gets.chomp
+  p "Enter the item's price #{item['price']}"
+  the_params[:price] = gets.chomp
+  p "Enter the item's description #{item['description']}"
+  the_params[:description] = gets.chomp
+  p "Enter the item's image #{item['image']}"
+  the_params[:image] = gets.chomp
+  response = Unirest.patch("localhost:3000/items/#{item_id}", parameters: the_params)
+  pp response.body
+elsif user_input == '5'
+  p "Enter the id of the product you would like to delete."
+  item_id = gets.chomp
+  response = Unirest.delete("localhost:3000/items/#{item_id}")
+  pp response.body
 end
